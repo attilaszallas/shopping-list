df73f514d97ea3166b37d12ac24e8687996b07cc
Attila Szallas <ASzallas@slb.com>
2024-02-07 12:51:56 +0100

    Modify Home Controller

--- ShoppingList/ShoppingList/Controllers/HomeController.cs
+++ ShoppingList/ShoppingList/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingList.Data;
 using ShoppingList.Models;
 using System.Diagnostics;
 
@@ -7,15 +9,147 @@ namespace ShoppingList.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ShoppingListDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ShoppingListDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: ShoppingItems
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ShoppingItems.ToListAsync());
+        }
+
+        // GET: ShoppingItems/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shoppingItem = await _context.ShoppingItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shoppingItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(shoppingItem);
+        }
+
+        // GET: ShoppingItems/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: ShoppingItems/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,IsPurchased")] ShoppingItem shoppingItem)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(shoppingItem);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shoppingItem);
+        }
+
+        // GET: ShoppingItems/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shoppingItem = await _context.ShoppingItems.FindAsync(id);
+            if (shoppingItem == null)
+            {
+                return NotFound();
+            }
+            return View(shoppingItem);
+        }
+
+        // POST: ShoppingItems/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,IsPurchased")] ShoppingItem shoppingItem)
+        {
+            if (id != shoppingItem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(shoppingItem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ShoppingItemExists(shoppingItem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(shoppingItem);
+        }
+
+        // GET: ShoppingItems/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var shoppingItem = await _context.ShoppingItems
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (shoppingItem == null)
+            {
+                return NotFound();
+            }
+
+            return View(shoppingItem);
+        }
+
+        // POST: ShoppingItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var shoppingItem = await _context.ShoppingItems.FindAsync(id);
+            if (shoppingItem != null)
+            {
+                _context.ShoppingItems.Remove(shoppingItem);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ShoppingItemExists(int id)
+        {
+            return _context.ShoppingItems.Any(e => e.Id == id);
         }
 
         public IActionResult Privacy()

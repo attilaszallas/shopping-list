5db095c39bcacdb64eda343ea6d4956f4e91c1e3
Attila Szallas <ASzallas@slb.com>
2024-02-07 12:09:53 +0100

    Add Model

---
+++ ShoppingList/ShoppingList/Models/ShoppingItem.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingList.Models
+{
+    public class ShoppingItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsPurchased { get; set; }
+    }
+}
